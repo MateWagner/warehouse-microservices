b73f6193168bbedc1fcb8f58ffa96506d912c65d
MateWagner <wwagnermate@gmail.com>
2023-12-13 09:24:37 +0100

    feat: change atomic address creation logic fix: wrong tests
    Signed-off-by: MateWagner <wwagnermate@gmail.com>

--- order-payment/src/main/java/com/codecool/order_payment/service/CityService.java
+++ order-payment/src/main/java/com/codecool/order_payment/service/CityService.java
@@ -3,9 +3,9 @@ package com.codecool.order_payment.service;
 import com.codecool.order_payment.modell.City;
 import com.codecool.order_payment.repository.CityRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,20 +13,13 @@ public class CityService {
     private final CityRepository cityRepository;
 
     protected City createAndOrGetCity(String city) {
-        try {
-            return getByCity(city);
-        } catch (HttpClientErrorException e) {
-            City newCity = City.builder().city(city).build();
-            return cityRepository.save(newCity);
-        }
+        Optional<City> storedCity = cityRepository.findByCity(city);
+
+        return storedCity.orElseGet(() -> createNewCity(city));
     }
 
-    private City getByCity(String city) {
-        return cityRepository.findByCity(city)
-                .orElseThrow(() -> new HttpClientErrorException(
-                                HttpStatus.NOT_FOUND,
-                                "Can't find City: " + city
-                        )
-                );
+    private City createNewCity(String city) {
+        City newCity = City.builder().city(city).build();
+        return cityRepository.save(newCity);
     }
 }
