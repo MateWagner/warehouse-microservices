0a0fce7e23cfb292649bbe8d21cc569191091928
MateWagner <wwagnermate@gmail.com>
2024-01-22 13:03:45 +0100

    feat: add missing @Repository annotation
    Signed-off-by: MateWagner <wwagnermate@gmail.com>

--- checkout-service/src/main/java/com/codecool/checkout/repository/jpa/OrderItemRepository.java
+++ checkout-service/src/main/java/com/codecool/checkout/repository/jpa/OrderItemRepository.java
@@ -2,6 +2,8 @@ package com.codecool.checkout.repository.jpa;
 
 import com.codecool.checkout.modell.jpa.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- checkout-service/src/main/java/com/codecool/checkout/repository/jpa/StreetRepository.java
+++ checkout-service/src/main/java/com/codecool/checkout/repository/jpa/StreetRepository.java
@@ -2,9 +2,11 @@ package com.codecool.checkout.repository.jpa;
 
 import com.codecool.checkout.modell.jpa.Street;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StreetRepository extends JpaRepository<Street, Long> {
     Optional<Street> findByStreet(String street);
 }
